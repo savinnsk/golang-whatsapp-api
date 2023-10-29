a2b10d2cc69c891c19eed497fd877fd63df5ef7a
savinnsk <saviodocuments@gmail.com>
2023-10-29 18:51:42 -0300

    refactor: move redis to infra

---
+++ internal/infra/redis/redis-client.go
@@ -0,0 +1,28 @@
+package infra
+
+import (
+	"context"
+
+	"github.com/go-redis/redis/v8"
+)
+
+var redisClient *redis.Client
+
+
+
+func InitializeRedis() *redis.Client {
+    redisClient = redis.NewClient(&redis.Options{
+        Addr:     "localhost:6379", // Endereço do servidor Redis
+        Password: "",              // Senha, se necessário
+        DB:       0,               // Número do banco de dados Redis
+    })
+
+    // Teste a conexão com o Redis
+    _, err := redisClient.Ping(context.Background()).Result()
+    if err != nil {
+        panic(err)
+    }
+
+	return redisClient
+
+}
