161daa8f95b6a5bb0c2ae2000629f879864a5368
savinnsk <saviodocuments@gmail.com>
2023-10-30 15:51:40 -0300

    feat: add inti dialog

--- internal/usecase/init.go
+++ internal/usecase/init.go
@@ -2,6 +2,8 @@ package usecase
 
 import (
 	"context"
+	"fmt"
+	"time"
 
 	"github.com/go-redis/redis/v8"
 	infra "github.com/savinnsk/prototype_bot_whatsapp/internal/infra/whatsmeow"
@@ -9,53 +11,86 @@ import (
 	"go.mau.fi/whatsmeow/types/events"
 )
 
-func Init(client *whatsmeow.Client, evt *events.Message, redisClient *redis.Client) {
+func Init(client *whatsmeow.Client, evt *events.Message, redisClient *redis.Client, currentChatId string) {
 
-	msg := `*Olá! Por favor, escolha uma das seguintes opções:*
+	if evt.Message.GetConversation() == "1" {
+		msg := `*Seus Agendamentos Abaixo:*
 
-	1. Deseja Agendar Horário? 📅
-	2. Ver Preços? 💲
-	3. Verificar seu Horário? ⏰
-	4. Ligar para atendente? 📞
-	5. Sair. 🚪
- 
-	
-	Responda com o número correspondente à sua escolha.`
+		1 - *HORA* : 13:00 - *DATA* 12/12/25 🕥
+		2 - *HORA* : 13:00 - *DATA* 12/12/25 🕥
 
-	chatNumber := infra.WhatsmeowSendResponse(client, evt, msg)
-	result, _ := redisClient.HSet(context.Background(), chatNumber, "currentChatId").Result()
-	// currentChatId, _ := redisClient.HGet(context.Background(), evt.Info.Chat.String(), "currentChatId").Result()
-	// if result != "" && currentChatId != "Initial" {
-	//     fmt.Println("Has a active conversation ")
-	//     return
+	  
+		0 - VOLTAR ? ⬅️`
 
-	// }
-	switch res {
-	case "1":
+		redisClient.HSet(context.Background(), evt.Info.Chat.String(), "currentChatId", "SHOW_USER_SCHEDULE").Result()
+		infra.WhatsmeowSendResponse(client, evt, msg)
+		return
 
-		infra.WhatsmeowSendResponse(client, evt, `*Você escolheu a opção 1: Deseja Agendar Horário.*
-        
-        os seguintes horários estão disponíveis:
-
-        1 - 12:00am
-        2 - 15:00pm
-        
-        `)
-
-		infra.WhatsmeowSendResponse(client, evt, "*Por favor, responda com o número correspondente ao horário desejado.*")
-
-		evt.Message.Reset()
-	case "2":
-
-		infra.WhatsmeowSendResponse(client, evt, "Você escolheu a opção 2: Ver Preços.")
-	case "3":
-
-		infra.WhatsmeowSendResponse(client, evt, "Você escolheu a opção 3: Verificar seu Horário.")
-	case "4":
-
-		infra.WhatsmeowSendResponse(client, evt, "Você escolheu a opção 4: Ligar para atendente.")
-	case "5":
-
-		infra.WhatsmeowSendResponse(client, evt, "Você escolheu a opção 5: Sair.")
 	}
+
+	if evt.Message.GetConversation() == "2" {
+		msg := `*Todos Horários disponíveis Abaixo:*
+
+		2 - *13:00* Hoje 🕥
+		3 - *15:00* Hoje 🕥
+		
+		
+		1 - OUTRA DATA 📅
+		0 - VOLTAR ⬅️
+
+		_DIGITE ALGUMA OPÇÃO_`
+
+		redisClient.HSet(context.Background(), evt.Info.Chat.String(), "currentChatId", "NEW_SCHEDULE").Result()
+		infra.WhatsmeowSendResponse(client, evt, msg)
+		return
+
+	}
+
+	if evt.Message.GetConversation() == "3" {
+		msg := `*Qual dos seus horários você deseja cancelar?:*
+
+		1 - *HORA* : 13:00 - *DATA* 12/12/25 🕥
+		2 - *HORA* : 13:00 - *DATA* 12/12/25 🕥
+		0 - VOLTAR ⬅️
+		`
+
+		redisClient.HSet(context.Background(), evt.Info.Chat.String(), "currentChatId", "CANCEL_SCHEDULE").Result()
+		infra.WhatsmeowSendResponse(client, evt, msg)
+
+		return
+	}
+
+	if evt.Message.GetConversation() == "4" {
+		msg := `*Contato*
+		☎️ - Sávio +5522996043721
+		`
+
+		infra.WhatsmeowSendResponse(client, evt, msg)
+
+		return
+	}
+
+	fields := map[string]interface{}{
+		"phone":         evt.Info.Chat.String(),
+		"currentChatId": "INIT",
+	}
+
+	err := redisClient.HMSet(context.Background(), evt.Info.Chat.String(), fields).Err()
+	if err != nil {
+		fmt.Println("Erro to save and init conversation:", err)
+	}
+	expirationDuration := 10 * time.Minute
+	redisClient.Expire(context.Background(), evt.Info.Chat.String(), expirationDuration).Result()
+
+	msg := `*Olá! Por favor, escolha uma das seguintes opções de 0 a 4:*
+
+	        1. VER SEU AGENDAMENTO ? 👀
+	        2. VER HORÁRIOS DISPONÍVEIS ? 👀
+	        3. CANCELAR UM AGENDAMENTO ? ❌
+	        4. ENTRAR EM CONTATO ? 📞
+
+	         _Responda com o número correspondente à sua escolha._`
+
+	infra.WhatsmeowSendResponse(client, evt, msg)
+
 }
